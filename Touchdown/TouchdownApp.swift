b33218ce6514d4f9c6c90d8550d731cbdbc1d560
Satya Prakash Sahu <satyasahu79@gmail.com>
2021-08-13 00:49:58 +0530

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Satya Prakash Sahu on 13/08/21.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
